675ad69db32f2485ee7e47db5db489e4c223b416
olivier <olibitter@gmail.com>
2023-12-02 18:51:38 +0100

    Optimize compinit

--- zsh/src/completion.zsh
+++ zsh/src/completion.zsh
@@ -1,12 +1,33 @@
-ZSH_COMPLETION_DIR="$XDG_DATA_HOME/zsh/completion"
+function() {
 
-[[ -d "$ZSH_COMPLETION_DIR" ]] || mkdir -p "$ZSH_COMPLETION_DIR"
-fpath+="$ZSH_COMPLETION_DIR"
+    local completion_dir="$XDG_DATA_HOME/zsh/completion"
 
-# cmd_exists gh \
-#     && [[ ! -f $completion_dir/_gh ]] 
-#     && gh completion -s zsh > ~/.local/share/zsh/completion/_gh
+    [[ -d "$completion_dir" ]] || mkdir -p "$completion_dir"
+    fpath+="$completion_dir"
 
-autoload -Uz compinit
-compinit -i
+    cmd_exists gh \
+        && [[ ! -f $completion_dir/_gh ]] \
+        && gh completion -s zsh > $completion_dir/_gh
 
+    local zsh_cache_dir="$XDG_CACHE_HOME/zsh"
+    if [[ ! -d $zsh_cache_dir ]]; then
+        echo "Creating $zsh_cache_dir"
+        mkdir -p $zsh_cache_dir
+    fi
+    local zshcompdump="$zsh_cache_dir/zcompdump"
+
+    autoload -Uz compinit
+
+    # cache .zcompdump for about a day
+    if [[ $zshcompdump(#qNmh-20) ]]; then
+        compinit -C -d "$zshcompdump"
+    else
+        compinit -i -d "$zshcompdump"; touch "$zshcompdump"
+    fi
+    {
+        # compile .zcompdump
+        if [[ -s "$zshcompdump" && (! -s "${zshcompdump}.zwc" || "$zshcompdump" -nt "${zshcompdump}.zwc") ]]; then
+            zcompile "$zshcompdump"
+        fi
+    } &!
+}
